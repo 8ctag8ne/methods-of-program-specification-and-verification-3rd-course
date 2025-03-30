4bb4018844dbdeef6783aca06a4ee5955eed8c40
Cumboy228 <nazaryagotin@gmail.com>
2025-03-30 10:13:30 +0300

    refactoring + gitignore

--- lab-1/obj/Debug/net9.0/lab-1.AssemblyInfo.cs
+++ lab-1/obj/Debug/net9.0/lab-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9cfb03d0e18c938f5317c61513d48d18b7196dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8934d1ad8d4e7cf95f94076cc8f53573bb90b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
